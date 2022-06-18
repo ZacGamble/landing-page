712e467bf47c782bc2b41e9be35052365e53d38b
ZacGamble <zacrgamble@gmail.com>
2022-06-17 22:28:16 -0600

    addeventlistener window.scrollY events working

---
+++ main.js
@@ -0,0 +1,44 @@
+let scrollPosition = window.scrollY
+const projectBanner = document.querySelector("#project-banner")
+const projects = document.querySelectorAll(".project-wrapper")
+// @ts-ignore
+const projectBannerHeight = projectBanner?.offsetHeight + 200
+
+const addClassOnScroll = () => projectBanner?.classList.add("project-header")
+const removeClassOnScroll = () => projectBanner?.classList.remove("project-header")
+const addProjectsOnScroll = () => projects?.forEach(p => p.classList.add("project-anim"))
+const removeProjectsOnScroll = () => projects?.forEach(p => p.classList.remove("project-anim"))
+window.addEventListener('scroll', function() {
+    scrollPosition = window.scrollY;
+    if(scrollPosition >= projectBannerHeight) {
+        addClassOnScroll()
+        addProjectsOnScroll()
+    }
+    else{
+        removeClassOnScroll()
+        removeProjectsOnScroll()
+    }
+})
+
+const skillsBanner = document.querySelector("#skills-banner")
+const skills = document.querySelectorAll(".skills")
+// @ts-ignore
+const skillsBannerHeight = skillsBanner.getBoundingClientRect().top + document.documentElement.scrollTop - 300
+
+const addSkillsBannerOnScroll = () => skillsBanner?.classList.add("skills-header")
+const removeSkillsBannerOnScroll = () => skillsBanner?.classList.remove("skills-header")
+const addSkilsOnScroll = () => skills.forEach(s => s.classList.add("skills-anim"))
+const removeSkilsOnScroll = () => skills.forEach(s => s.classList.remove("skills-anim"))
+window.addEventListener('scroll', function() {
+    scrollPosition = window.scrollY;
+    if(scrollPosition >= skillsBannerHeight) {
+        addSkillsBannerOnScroll()
+        addSkilsOnScroll()
+        }
+    else{
+        removeSkillsBannerOnScroll()
+        removeSkilsOnScroll()
+    }
+})
+
+console.log(skillsBannerHeight);
